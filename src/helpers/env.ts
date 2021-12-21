1a58ff861a8163353202560d3544d2d2546d04bf
iago.silva <iago.silva@lucaspaganini.com>
2021-12-21 20:07:54 -0300

    Improve env helper

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,25 +1,11 @@
-import * as t from "io-ts";
 import * as E from "fp-ts/Either";
 import { pipe } from "fp-ts/function";
 import { failure } from "io-ts/lib/PathReporter";
-import { withMessage } from "io-ts-types";
-
-type LengthBrand = {
-  readonly isNonEmptyString: unique symbol;
-};
-
-const isNonEmptyStringCodec = t.brand(
-  t.string,
-  (value): value is t.Branded<string, LengthBrand> => isNonEmptyString(value),
-  "isNonEmptyString"
-);
-
-const isNonEmptyString = (value: unknown) =>
-  typeof value === "string" && value.length > 0;
+import { withMessage, NonEmptyString } from "io-ts-types";
 
 export const env = (key: string) => {
   const envCodec = withMessage(
-    isNonEmptyStringCodec,
+    NonEmptyString,
     () => `You must set the var env ${key}`
   );
   return pipe(
