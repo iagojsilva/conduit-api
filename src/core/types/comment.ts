b814b05b717a528d8c8ac82ef51243f35f84ef14
iago.silva <iago.silva@lucaspaganini.com>
2021-12-21 21:14:24 -0300

    Refacor comment type

--- src/core/types/comment.ts
+++ src/core/types/comment.ts
@@ -1,6 +1,7 @@
 import { profileCodec } from "@/core/types/profile";
 import * as t from "io-ts";
 import { dateCodec } from "@/core/types/scalar";
+import { NonEmptyString, withMessage } from "io-ts-types";
 
 export const commentCodec = t.type({
   id: t.number,
@@ -11,3 +12,13 @@ export const commentCodec = t.type({
 });
 
 export type Comment = t.TypeOf<typeof commentCodec>;
+export type CommentOutput = t.OutputOf<typeof commentCodec>;
+
+export const creatableCommentCodec = t.type({
+  body: withMessage(
+    NonEmptyString,
+    () => "The body of the comment must not be a empty string"
+  ),
+});
+
+export type CreatableComment = t.TypeOf<typeof creatableCommentCodec>;
